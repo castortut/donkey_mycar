318d0e3084638e54f2853ea4fe4107a8a61a9be7
Castor Jetson Nano <jetson@avaruuskerho.fi>
2019-10-22 23:30:20 +0300

    Tune PWM values

--- myconfig.py
+++ myconfig.py
@@ -46,9 +46,9 @@ STEERING_RIGHT_PWM = 440    #pwm value for full right steering
                                                                
 # THROTTLE                                                     
 THROTTLE_CHANNEL = 0        #channel on the 9685 pwm board 0-15
-THROTTLE_FORWARD_PWM = 300  #pwm value for max forward throttle
+THROTTLE_FORWARD_PWM = 330  #pwm value for max forward throttle
 THROTTLE_STOPPED_PWM = 420  #pwm value for no movement
-THROTTLE_REVERSE_PWM = 550  #pwm value for max reverse throttle
+THROTTLE_REVERSE_PWM = 560  #pwm value for max reverse throttle
 # 
 # #DC_STEER_THROTTLE with one motor as steering, one as drive
 # #these GPIO pinouts are only used for the DRIVE_TRAIN_TYPE=DC_STEER_THROTTLE
@@ -117,7 +117,7 @@ THROTTLE_REVERSE_PWM = 550  #pwm value for max reverse throttle
 # JOYSTICK_MAX_THROTTLE = 0.5         #this scalar is multiplied with the -1 to 1 throttle value to limit the maximum throttle. This can help if you drop the controller or just don't need the full speed available.
 # JOYSTICK_STEERING_SCALE = 1.0       #some people want a steering that is less sensitve. This scalar is multiplied with the steering -1 to 1. It can be negative to reverse dir.
 # AUTO_RECORD_ON_THROTTLE = True      #if true, we will record whenever throttle is not zero. if false, you must manually toggle recording with some other trigger. Usually circle button on joystick.
-# CONTROLLER_TYPE='ps3'               #(ps3|ps4|xbox|nimbus|wiiu|F710|rc3)
+CONTROLLER_TYPE='rc3'               #(ps3|ps4|xbox|nimbus|wiiu|F710|rc3)
 # USE_NETWORKED_JS = False            #should we listen for remote joystick control over the network?
 # NETWORK_JS_SERVER_IP = "192.168.0.1"#when listening for network joystick control, which ip is serving this information
 # JOYSTICK_DEADZONE = 0.0             # when non zero, this is the smallest throttle before recording triggered.
